7935f662928fc60b5f00bd4f4ac27a82089c3c4e
1322WHYBAN <whybanmemr6969@outlook.com>
2024-12-17 15:30:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7283606759:AAHsFEQGI50beuYQN1kTICJyum9xGcHdotY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@P1dermanxd")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1553853316")
+
+
+
+
+
+
+
+  
